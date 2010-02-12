1ac66738c86c5fdf10903a3357ebfe9510c6c07c
Daniel O'Connor <clockwerx@php.net>
2010-02-12 03:47:39 +0000

    Fixed PHPUnit Compatibility
    git-svn-id: http://svn.php.net/repository/pear/packages/Text_Statistics/trunk@294971 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -3,7 +3,6 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'Text_Statistics_AllTests::main');
 }
 
-require_once 'PHPUnit/Framework/TestSuite.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
 chdir(dirname(__FILE__) . '/../');
